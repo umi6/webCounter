03b3c4178c34b515abac2372761b313eeb419afa
umi6 <umi6@users.noreply.github.com>
2026-05-04 05:15:59 +0000

    deploy: 90eb316de2f02cc973362e2364549cf355220732

--- webCounter/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-const base = process.env.VITE_APP_BASE || '/webCounter/'
-
-export default defineConfig({
-  base,
-  plugins: [react()],
-})
